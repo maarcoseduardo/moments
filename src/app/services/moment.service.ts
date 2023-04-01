8606b826a3556f5d43ed2b863f74e6041be28a30
Marcos Eduardo <maarcosefb0@gmail.com>
2023-04-01 12:41:31 -0300

    method created moment by services its OK

--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { IMoment } from '../@types/Moments';
+import { enviroment } from 'src/enviroments/enviroment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MomentService {
+  private baseApiUrl = enviroment.baseApiUrl;
+  private apiUrl = `${this.baseApiUrl}/api/moments`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  createMoment(formData: FormData): Observable<FormData> {
+    return this.http.post<FormData>(this.apiUrl, formData);
+  }
 }
