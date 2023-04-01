0fff59d88f2d059b1189f4dec3417643ba4afb7b
Marcos Eduardo <maarcosefb0@gmail.com>
2023-04-01 15:17:07 -0300

    chore: add component messages after shared moments and add styles on component messages

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,29 +1,39 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { IMoment } from 'src/app/@types/Moments';
 
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
-  styleUrls: ['./new-moment.component.scss']
+  styleUrls: ['./new-moment.component.scss'],
 })
 export class NewMomentComponent {
-  btnText = "Compartilhar";
+  btnText = 'Compartilhar';
 
-  constructor(private momentService: MomentService) {}
+  constructor(
+    private momentService: MomentService,
+    private messageService: MessagesService,
+    private router: Router
+  ) {}
 
-  async createHandler(moment: IMoment){
-    const formData = new FormData()
+  async createHandler(moment: IMoment) {
+    const formData = new FormData();
 
-    formData.append("title", moment.title)
-    formData.append("description", moment.description)
-    
-    if(moment.image ){
-      formData.append("image", moment.image)
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
     }
 
     await this.momentService.createMoment(formData).subscribe();
+    this.messageService.add('Momento adicionado com sucesso!');
 
+    setTimeout(() => {
+      this.router.navigate(['/']);
+    }, 4500)
   }
 }
